0830222c34611579a8bd124e22bf8f4f355c359c
Tulsi Gehlot <tulsi@kloudone.com>
2022-09-01 17:35:27 +0530

    new changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -15,20 +15,25 @@ function Navbar() {
            </div>
             <div className="nav-list">
                 <ul className={isActive ? "active-list" : "navlist"}>
-                    <NavLink to='/'>Home</NavLink>
-                    <NavLink to='/dresses'>Dresses</NavLink>
-                    <NavLink to='/shoes'>Shoes</NavLink>
-                    <NavLink to='/bags'>Bags</NavLink>
-                    <NavLink to='/watches'>Watches</NavLink>
-                    <NavLink to='/sunglasses'>Sunglasses</NavLink>
-                    <NavLink to='/jewellery'>Jewellery</NavLink>
-                    <NavLink to='/contact'>Contact</NavLink>
+                    <NavLink to='/'><img src="/images/favicon/home.png" alt="" /></NavLink>
+                    <NavLink to='/dresses'><img src="/images/favicon/dress.png" alt="" /></NavLink>
+                    <NavLink to='/shoes'><img src="/images/favicon/high-heel.png" alt="" /></NavLink>
+                    <NavLink to='/bags'><img src="/images/favicon/shopping-bag.png" alt="" /></NavLink>
+                    <NavLink to='/watches'><img src="/images/favicon/hand-watch.png" alt="" /></NavLink>
+                    <NavLink to='/sunglasses'><img src="/images/favicon/sunglasses.png" alt="" /></NavLink>
+                    <NavLink to='/jewellery'><img src="/images/favicon/necklace.png" alt="" /></NavLink>
+                    <NavLink to='/contact'><img src="/images/favicon/phone.png" alt="" /></NavLink>
                 </ul>
             </div>
 
+
             <div className='hamburger-menu' onClick={() => setIsActive(!isActive)} >
-            <i class='bx bx-menu'></i>
-              
+              <i class='bx bx-menu'></i>
+            </div>
+
+            
+            <div className="cart_btn">
+              <img src="/images/favicon/shopping-cart.png" alt="" />
             </div>
         </div>
     </div>
